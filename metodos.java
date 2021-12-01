2f9063a8257b59eae329034c8dcf1c6141528d9b
IvanMorell <imsmorsell2002@gmail.com>
2021-12-01 22:57:20 +0100

    Metodos de randomizacion

---
+++ metodos.java
@@ -0,0 +1,28 @@
+public static int puntosCPU() {
+   	System.out.println("CPU lanza el dado al aire ...");
+        Thread.sleep(DELAY);                        
+        tiradaCPU = numeroAleatorio(6,1);
+        System.out.println("\n?????");               
+        System.out.println("? " + tiradaCPU + " ?");               
+        System.out.println("?????\n");                        
+        System.out.println("La puntuación actual de la CPU es " +puntosCPU);
+        return puntosCPU;
+   }
+   
+   public static int puntosJugador() {
+   	System.out.println("Jugador lanza el dado al aire ...");
+        Thread.sleep(DELAY);
+        tiradaPlayer = numeroAleatorio(6,1);
+        System.out.println("\n*****");               
+        System.out.println("* " + tiradaPlayer + " *");               
+        System.out.println("*****");               
+        puntosPlayer += tiradaPlayer;               
+        System.out.println("\nTu puntuación actual es " + puntosPlayer);
+        return puntosPlayer;
+   }
+   
+   public static int numeroAleatorio(int max, int min) {
+   	Random r = new Random();
+   	int numAlea = r.nextInt(max - min + 1) + min;
+   	return numAlea;
+   }
